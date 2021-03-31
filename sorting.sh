397ea838e4d6d80737f90131981b342d96f7c647
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-03-31 14:29:43 +0530

    Added: compute program

--- sorting.sh
+++ sorting.sh
@@ -2,3 +2,4 @@ echo "Welcome to sorting Arithmetic computation program"
 read -p "Enter first input " a
 read -p "Enter second input " b
 read -p "Enter third input " c
+echo 'compute1=%0.0f' "$(( a +b * c ))"
